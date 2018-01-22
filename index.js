f1c671e0e58f91746675a212d1cf97b80b60bc00
Sang Woo Moon <swmoonnz@gmail.com>
2018-01-22 07:19:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, day) {
+  return `It's ${days} days until ${holiday}!`
+}
